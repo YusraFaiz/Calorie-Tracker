e7e2c0fb6eaa12b797c89a2a8dc76653bb5ef138
YusraFaiz <yusra.faiz8@gmail.com>
2021-06-04 10:26:55 -1100

    commit

---
+++ mysite/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Food,Consume
+# Register your models here.
+
+admin.site.register(Food)
+admin.site.register(Consume)
+
+
---
+++ mysite/myapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Food(models.Model):
+
+    def __str__(self):
+        return self.name
+        
+    name = models.CharField(max_length=200)
+    cabs = models.FloatField()
+    protien = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
+
+class Consume(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food,on_delete = models.CASCADE)
+
